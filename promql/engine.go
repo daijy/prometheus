c15ca778a05821ac2f9975d65c6f78529fc7a773
Daniel Dai <jidai@roku.com>
2025-09-05 13:11:57 -0700

    print type of s

--- promql/engine.go
+++ promql/engine.go
@@ -1736,6 +1736,9 @@ func (ev *evaluator) eval(ctx context.Context, expr parser.Expr) (parser.Value, 
 
 		for i, s := range selVS.Series {
 			//ev.logger.Info("Begin Series " + fmt.Sprintf("%d: %s", i, s.Labels().String()))
+			if i == 1 {
+				ev.logger.Info("jidai9 type of s %T", s)
+			}
 			totalSeries++
 			if err := contextDone(ctx, "expression evaluation"); err != nil {
 				ev.error(err)
