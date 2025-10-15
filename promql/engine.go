cf828799c4dcd6d1a0bf10de5edbdcf36b3ad0c4
Daniel Dai <jidai@roku.com>
2025-10-15 13:56:18 -0700

    Better logging

--- promql/engine.go
+++ promql/engine.go
@@ -498,7 +498,6 @@ func (ng *Engine) NewInstantQuery(ctx context.Context, q storage.Queryable, opts
 // NewRangeQuery returns an evaluation query for the given time range and with
 // the resolution set by the interval.
 func (ng *Engine) NewRangeQuery(ctx context.Context, q storage.Queryable, opts QueryOpts, qs string, start, end time.Time, interval time.Duration) (Query, error) {
-	log.Println("NewRangeQuery")
 	pExpr, qry := ng.newQuery(q, qs, opts, start, end, interval)
 	finishQueue, err := ng.queueActive(ctx, qry)
 	if err != nil {
@@ -620,6 +619,7 @@ func (ng *Engine) NewTestQuery(f func(context.Context) error) Query {
 // At this point per query only one EvalStmt is evaluated. Alert and record
 // statements are not handled by the Engine.
 func (ng *Engine) exec(ctx context.Context, q *query) (v parser.Value, ws annotations.Annotations, err error) {
+	log.Println("prometheus exec %s", q.q)
 	ng.metrics.currentQueries.Inc()
 	defer func() {
 		ng.metrics.currentQueries.Dec()
