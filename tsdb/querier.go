a7b1a5f3db2fbbb6fa4a704f6d86003ec474d917
Daniel Dai <jidai@roku.com>
2025-11-01 22:40:24 -0700

    logging

--- tsdb/querier.go
+++ tsdb/querier.go
@@ -543,10 +543,10 @@ func (b *blockBaseSeriesSet) Next() bool {
 		// Prefilter chunks and pick those which are not entirely deleted or totally outside of the requested range.
 		log.Printf("jidai p.Next %d, %t", len(b.bufChks), b.disableTrimming)
 		for _, chk := range b.bufChks {
-			if chk.MaxTime < b.mint {
-				log.Printf("jidai here1, %d, %d", chk.MaxTime, b.mint)
-				continue
-			}
+			// if chk.MaxTime < b.mint {
+			// 	log.Printf("jidai here1, %d, %d", chk.MaxTime, b.mint)
+			// 	continue
+			// }
 			if chk.MinTime > b.maxt {
 				log.Println("jidai here2")
 				continue
