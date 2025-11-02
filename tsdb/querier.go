7c68707d99e56f93d18aab84275a325f69169100
Daniel Dai <jidai@roku.com>
2025-11-01 17:15:46 -0700

    logging

--- tsdb/querier.go
+++ tsdb/querier.go
@@ -513,7 +513,6 @@ func (b *blockBaseSeriesSet) Next() bool {
 			return false
 		}
 
-		log.Printf("jidai p.Next %d", len(b.bufChks))
 		if len(b.bufChks) == 0 {
 			continue
 		}
@@ -542,14 +541,18 @@ func (b *blockBaseSeriesSet) Next() bool {
 		chks := make([]chunks.Meta, 0, nChks)
 
 		// Prefilter chunks and pick those which are not entirely deleted or totally outside of the requested range.
+		log.Printf("jidai p.Next %d", len(b.bufChks))
 		for _, chk := range b.bufChks {
 			if chk.MaxTime < b.mint {
+				log.Println("jidai here1")
 				continue
 			}
 			if chk.MinTime > b.maxt {
+				log.Println("jidai here2")
 				continue
 			}
 			if (tombstones.Interval{Mint: chk.MinTime, Maxt: chk.MaxTime}.IsSubrange(intervals)) {
+				log.Println("jidai here3")
 				continue
 			}
 			chks = append(chks, chk)
